8643da4c902d78f10de9f38d3b3e4e0e150b1645
Yana Pavlyk <pavlikyg@gmail.com>
2022-07-08 23:24:43 +0300

    fixed task 4

--- js/task-04.js
+++ js/task-04.js
@@ -1,20 +1,39 @@
-const counterValue = document.querySelector("#value")
+// const counterValue = document.querySelector("#value")
+// const decrementBtn = document.querySelector('[data-action="decrement"]')
+// const incrementtBtn = document.querySelector('[data-action="increment"]')
+
+// let currentCounterValue = Number(counterValue.textContent)
+
+// const onDecrementBtnClick = () => {
+//     currentCounterValue += Number(decrementBtn.textContent)
+//     counterValue.innerHTML = currentCounterValue
+// }
+
+// const onIncrementBtnClick = () => {
+//     currentCounterValue += Number(incrementtBtn.textContent)
+//     counterValue.innerHTML = currentCounterValue
+// }
+
+// decrementBtn.addEventListener("click", onDecrementBtnClick)
+// incrementtBtn.addEventListener("click", onIncrementBtnClick)
+
+
+let counterValue = 0
+const value = document.querySelector("#value")
+value.innerHTML = counterValue
+
 const decrementBtn = document.querySelector('[data-action="decrement"]')
 const incrementtBtn = document.querySelector('[data-action="increment"]')
 
-let currentCounterValue = Number(counterValue.textContent)
-
 const onDecrementBtnClick = () => {
-    currentCounterValue += Number(decrementBtn.textContent)
-    counterValue.innerHTML = currentCounterValue
+    counterValue += Number(decrementBtn.textContent)
+    value.innerHTML = counterValue
 }
 
 const onIncrementBtnClick = () => {
-    currentCounterValue += Number(incrementtBtn.textContent)
-    counterValue.innerHTML = currentCounterValue
+    counterValue += Number(incrementtBtn.textContent)
+    value.innerHTML = counterValue
 }
 
 decrementBtn.addEventListener("click", onDecrementBtnClick)
 incrementtBtn.addEventListener("click", onIncrementBtnClick)
-
-
