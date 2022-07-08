07254b040c5b4cd179b6f95d472682e5b24bc866
Yana Pavlyk <pavlikyg@gmail.com>
2022-07-08 17:54:26 +0300

    task 4 done

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,20 @@
+const counterValue = document.querySelector("#value")
+const decrementBtn = document.querySelector('[data-action="decrement"]')
+const incrementtBtn = document.querySelector('[data-action="increment"]')
+
+let currentCounterValue = Number(counterValue.textContent)
+
+const onDecrementBtnClick = () => {
+    currentCounterValue += Number(decrementBtn.textContent)
+    counterValue.innerHTML = currentCounterValue
+}
+
+const onIncrementBtnClick = () => {
+    currentCounterValue += Number(incrementtBtn.textContent)
+    counterValue.innerHTML = currentCounterValue
+}
+
+decrementBtn.addEventListener("click", onDecrementBtnClick)
+incrementtBtn.addEventListener("click", onIncrementBtnClick)
+
 
