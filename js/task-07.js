8bab7828cc1f337205a846665ac07e8534e7c996
Yana Pavlyk <pavlikyg@gmail.com>
2022-07-09 21:58:17 +0300

    task 7 fixed

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,9 @@
 const text = document.querySelector("#text")
 const slider = document.querySelector("#font-size-control")
 
+//задаємо стартовий розмір шрифта відповідно до початкового положення шкали
+text.style.fontSize = `${slider.value}px` 
+
 const onInputMove = (event) => {
 
     text.style.fontSize = `${event.currentTarget.value}px`
