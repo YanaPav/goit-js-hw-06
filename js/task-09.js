03629b5b107c6ae766ac03b056697bd8c4eee17e
Yana Pavlyk <pavlikyg@gmail.com>
2022-07-09 22:49:27 +0300

    task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.body
+const changeColorBtn = document.querySelector('.change-color')
+const colorText = document.querySelector('.color')
+
+
+const onChangeColorBtnClick = () => {
+  const currentColor = getRandomHexColor()
+  body.style.backgroundColor = currentColor
+  colorText.textContent = currentColor
+}
+
+changeColorBtn.addEventListener('click', onChangeColorBtnClick)
