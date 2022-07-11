46b113af69e9b3e9a1b24bf5de20051fc5ac5060
Yana Pavlyk <pavlikyg@gmail.com>
2022-07-11 21:54:14 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -3,8 +3,13 @@ console.log(`Number of categories: ${categoriesCount.length}`)
 
 // Можна продовжити працювати з псевдомасивом, але мені більше подобаються справжні, тому створюю нову змінну
 
-const categoriesArr = [...categoriesCount]
-for (const category of categoriesArr) {
+// const categoriesArr = [...categoriesCount]
+// for (const category of categoriesArr) {
+//     console.log('Category:', category.querySelector('h2').textContent)    
+//     console.log(`Elements: ${category.querySelectorAll('li').length}`)
+// }
+
+categoriesCount.forEach(category => {
     console.log('Category:', category.querySelector('h2').textContent)    
     console.log(`Elements: ${category.querySelectorAll('li').length}`)
-}
+})
